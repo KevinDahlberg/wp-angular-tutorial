958f10fc8b03a87908cff483c92938d934fb91fc
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-07-14 20:10:49 -0500

    sourced files in functions.php

---
+++ functions.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* Enqueue scripts and styles
+*/
+
+function scripts() {
+
+	// load angular-material css
+	wp_enqueue_style( 'angular-material', get_template_directory_uri() . '/node_modules/angular-material/angular-material.css' );
+
+	// load Font Awesome css
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/includes/css/font-awesome.min.css', false, '4.1.0' );
+
+	// load stylesheet
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+  //load angular
+  wp_enqueue_script('angularjs', get_template_directory_uri() .'/node_modules/angular/angular.min.js');
+  wp_enqueue_script('angularjs-route', get_template_directory_uri() .'/node_modules/angular-route/angular-route.min.js');
+  wp_enqueue_script('angular-animate', get_template_directory_uri() .'/node_modules/angular-animate/angular-animate.min.js');
+  wp_enqueue_script('angular-aria', get_template_directory_uri() .'/node_modules/angular-aria/angular-aria.min.js');
+  wp_enqueue_script('angular-materialjs', get_template_directory_uri() .'/node_modules/angular-material/angular-material.min.js');
+  wp_enqueue_script('angular-sanitize', get_template_directory_uri() .'/node_modules/angular-sanitize/angular-sanitize.min.js');
+
+  wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/scripts/config.js');
+
+	//classes
+
+	//load services
+
+  //load controllers
+
+  if ( is_user_logged_in() && current_user_can( 'edit_posts' ) ) {
+    wp_localize_script('RunService', 'WPsettings', array(
+      'root' => esc_url_raw( rest_url() ),
+      'nonce' => wp_create_nonce( 'wp_rest' ),
+      'current_ID' => get_the_ID(),
+      'session_token' => wp_get_session_token(),
+      'user_ID' => get_current_user_id()
+    ));
+  };
+
+  // With get_stylesheet_directory_uri()
+  wp_localize_script('scripts', 'localized',
+  array(
+		'partials' => get_stylesheet_directory_uri() . '/views/',
+		'url' => get_stylesheet_directory_uri() . '/'
+		));
+}
+
+add_action( 'wp_enqueue_scripts', 'scripts');
+
+?>
--- readme.md
+++ readme.md
@@ -1,1 +1,28 @@
 # Tutorial creating a To Do List App that combines AngularJS and the Wordpress REST API
+
+## Set Up File Structure
+
+2 necessary files for wordpress - style.css and the index.php
+
+header and footer
+
+functions.php
+
+- scripts folder, config.js file
+
+- views folder for the various views
+
+## NPM
+
+- npm init
+
+time to instal the angularJS files (and angular material, just because)
+
+npm install
+angular, angular-animate, angular-aria, angular-route, angular-sanitize, angular-material
+
+package.json
+
+## Setting Up Angular
+
+- functions.php - enqueue scripts, styles
